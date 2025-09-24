c44ec73c288708e9fd9e35a4560daaa300acc7c3
Menno <s1215063@student.windesheim.nl>
2025-09-24 19:40:30 +0200

    Register View added

---
+++ Grocery.App/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,33 @@
+﻿
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Grocery.Core.Interfaces.Services;
+using Grocery.Core.Models;
+
+namespace Grocery.App.ViewModels
+{
+    public partial class RegisterViewModel : BaseViewModel
+    {
+        private readonly IAuthService _authService;
+        private readonly GlobalViewModel _global;
+
+        [ObservableProperty]
+        private string email = "user3@mail.com";
+
+        [ObservableProperty]
+        private string password = "user3";
+
+        [ObservableProperty]
+        private string registerMessage;
+
+        public RegisterViewModel()
+        { //_authService = App.Services.GetServices<IAuthService>().FirstOrDefault();
+        }
+
+        [RelayCommand]
+        private void Register()
+        {
+            return;
+        }
+    }
+}
