c45ebc78284d412ea6ec9702e6edf8965827dddd
Menno <s1215063@student.windesheim.nl>
2025-09-24 18:53:33 +0200

    Search request gives the correct correlating result

--- Grocery.App/ViewModels/GroceryListItemsViewModel.cs
+++ Grocery.App/ViewModels/GroceryListItemsViewModel.cs
@@ -44,7 +44,7 @@ namespace Grocery.App.ViewModels
         {
             AvailableProducts.Clear();
             foreach (Product p in _productService.GetAll())
-                if (MyGroceryListItems.FirstOrDefault(g => g.ProductId == p.Id) == null  && p.Stock > 0)
+                if (MyGroceryListItems.FirstOrDefault(g => g.ProductId == p.Id) == null  && p.Stock > 0 && (searchedProduct == "" || p.Name.Contains(searchedProduct)))
                     AvailableProducts.Add(p);
         }
 
